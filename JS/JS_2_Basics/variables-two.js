6f8702a6f756c176d70c14299ac1720a7af59afb
Deep Das <deep.das@science.christuniversity.in>
2020-08-26 09:54:37 +0530

    Add files via upload

---
+++ JS/JS_2_Basics/variables-two.js
@@ -0,0 +1,21 @@
+const uid = "abc123";
+
+var fullName = "Deep Das";
+var email = "deepd1007@gmail.com";
+var pass = "123456";
+var confirm = "123456";
+var courseCount = 0;
+var isLoggedInFromGoogle = false;
+
+//fullName = prompt("Enter you name here");
+
+console.log(uid);
+console.log("Full Name:- " + fullName);
+console.log(email);
+
+console.log(`
+    User's Unique ID:- ${uid}
+    User's Name:- ${fullName}
+    User's Mail:- ${email}
+    User's Password: ${pass}
+`);
